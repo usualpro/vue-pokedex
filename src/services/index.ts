b12d00a97b139f19ba528b8d51842346bc39f376
Gary FELICITE-PRUDENT <usualpro@gmail.com>
2025-05-25 13:24:49 +0200

    localstorage implementation

--- src/services/index.ts
+++ src/services/index.ts
@@ -5,5 +5,10 @@ export const alovaInstance = createAlova({
   requestAdapter: adapterFetch(),
   responded: (response) => response.json(),
   baseURL: "https://pokeapi.co/api/v2",
+  cacheFor: {
+    GET: {
+      mode: "restore",
+      expire: () => 24 * 60 * 60, //1 day
+    },
+  },
 });
-
